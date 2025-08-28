c73a0824772658960acf6fae6d0ae0e67eb94b51
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-08-28 21:14:29 +0545

    setup Spring Boot project with MySQL, JPA, Security, Swagger, and JWT dependencies

---
+++ src/main/java/com/example/authtemplate/SpringAuthTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.authtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthTemplateApplication.class, args);
+    }
+
+}
